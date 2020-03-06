c36956a9d5dbee9fef6a2c9216dd082959a5df67
Aish11071998 <aishwarya.sekar@ducenit.com>
2020-03-06 14:53:44 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,31 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+//................................................
+const arr=[1,2,3,4,5]
+const objarray=[{id:'1'},{id:'2'}];
+for(let a of objarray)
+{
+  console.log(a)
+}
+arr.forEach((item,index)=>
+{
+  console.log(item)
+  console.log(index)
+ // console.log(arr)
+})
+
+const x=arr.map((a,i)=>{
+  console.log(i)
+  return a*2
+})
+console.log(x)
+
+
+const fil=arr.filter((num=>num>1)
+
+)
+console.log(fil)
+
+//
